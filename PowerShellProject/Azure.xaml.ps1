ee0fd21997f5ea1c471630d271be2c0fd0643d74
gethari <hharan618@gmail.com>
2019-11-29 10:13:22 +0530

    initial commit

---
+++ PowerShellProject/Azure.xaml.ps1
@@ -0,0 +1,113 @@
+﻿Add-Type -AssemblyName PresentationFramework
+function Add-ControlVariables {
+	
+New-Variable -Name 'Login' -Value $window.FindName('Login') -Scope 1 -Force	
+New-Variable -Name 'AvailableSubscriptions' -Value $window.FindName('AvailableSubscriptions') -Scope 1 -Force	
+New-Variable -Name 'AvailableResourceGroups' -Value $window.FindName('AvailableResourceGroups') -Scope 1 -Force	
+New-Variable -Name 'AvailableLogicApps' -Value $window.FindName('AvailableLogicApps') -Scope 1 -Force	
+New-Variable -Name 'xml' -Value $window.FindName('xml') -Scope 1 -Force	
+New-Variable -Name 'json' -Value $window.FindName('json') -Scope 1 -Force	
+New-Variable -Name 'MessageBody' -Value $window.FindName('MessageBody') -Scope 1 -Force	
+New-Variable -Name 'Trigger' -Value $window.FindName('Trigger') -Scope 1 -Force	
+New-Variable -Name 'Request' -Value $window.FindName('Request') -Scope 1 -Force	
+New-Variable -Name 'Manual' -Value $window.FindName('Manual') -Scope 1 -Force
+New-Variable -Name 'FileName' -Value $window.FindName('FileName') -Scope 1 -Force
+New-Variable -Name 'Result' -Value $window.FindName('Result') -Scope 1 -Force
+}
+
+function Load-Xaml {
+	[xml]$xaml = Get-Content -Path $PSScriptRoot\Azure.xaml
+	$manager = New-Object System.Xml.XmlNamespaceManager -ArgumentList $xaml.NameTable
+	$manager.AddNamespace("x", "http://schemas.microsoft.com/winfx/2006/xaml");
+	$xaml.SelectNodes("//*[@x:Name='Login']", $manager)[0].RemoveAttribute('Click')
+	$xaml.SelectNodes("//*[@x:Name='AvailableSubscriptions']", $manager)[0].RemoveAttribute('SelectionChanged')
+	$xaml.SelectNodes("//*[@x:Name='AvailableResourceGroups']", $manager)[0].RemoveAttribute('SelectionChanged')
+	$xaml.SelectNodes("//*[@x:Name='AvailableLogicApps']", $manager)[0].RemoveAttribute('SelectionChanged')
+	$xaml.SelectNodes("//*[@x:Name='xml']", $manager)[0].RemoveAttribute('Checked')
+	$xaml.SelectNodes("//*[@x:Name='json']", $manager)[0].RemoveAttribute('Checked')
+	$xaml.SelectNodes("//*[@x:Name='MessageBody']", $manager)[0].RemoveAttribute('Click')
+	$xaml.SelectNodes("//*[@x:Name='Trigger']", $manager)[0].RemoveAttribute('Click')
+	$xaml.SelectNodes("//*[@x:Name='Request']", $manager)[0].RemoveAttribute('Checked')
+	$xaml.SelectNodes("//*[@x:Name='Manual']", $manager)[0].RemoveAttribute('Checked')
+	$xaml.SelectNodes("//*[@x:Name='FileName']", $manager)[0].RemoveAttribute('Checked')
+	$xaml.SelectNodes("//*[@x:Name='Result']", $manager)[0].RemoveAttribute('Checked')
+	$xamlReader = New-Object System.Xml.XmlNodeReader $xaml
+	[Windows.Markup.XamlReader]::Load($xamlReader)
+}
+
+function Set-EventHandlers {
+
+	$Login.add_Click({
+		param([System.Object]$sender,[System.Windows.RoutedEventArgs]$e)
+		Connect-AzureRmAccount
+		$AvailableSubscriptions.ItemsSource = Get-AzureRmSubscription | Select-Object -ExpandProperty Name
+	})
+	$AvailableSubscriptions.add_SelectionChanged({
+		param([System.Object]$sender,[System.Windows.Controls.SelectionChangedEventArgs]$e)
+		$subscription = $sender.SelectedItem
+		Select-AzureRmSubscription -SubscriptionName $subscription
+		$AvailableResourceGroups.ItemsSource = [Collections.Generic.List[String]](Get-AzureRmResourceGroup | Select-Object -ExpandProperty ResourceGroupName)
+	})
+	$AvailableResourceGroups.add_SelectionChanged({
+		param([System.Object]$sender,[System.Windows.Controls.SelectionChangedEventArgs]$e)
+		$Global:resourceGroupName = $sender.SelectedItem
+		$AvailableLogicApps.ItemsSource = Get-AzureRmResource -ResourceType "Microsoft.Logic/workflows" -ResourceGroupName $resourceGroupName | Select-Object -ExpandProperty Name
+	})
+	$AvailableLogicApps.add_SelectionChanged({
+		param([System.Object]$sender,[System.Windows.Controls.SelectionChangedEventArgs]$e)
+		$Global:logicAppName = $sender.SelectedItem
+		$Global:url = Get-AzureRmLogicAppTriggerCallbackUrl -ResourceGroupName $resourceGroupName -Name $logicAppName -TriggerName $requestType | Select-Object -ExpandProperty Value
+	})
+	$xml.add_Checked({
+		param([System.Object]$sender,[System.Windows.RoutedEventArgs]$e)
+		$Global:contentType = $xml.Content
+	})
+	$json.add_Checked({
+		param([System.Object]$sender,[System.Windows.RoutedEventArgs]$e)
+		$Global:contentType = $json.Content
+	})
+	$MessageBody.add_Click({
+		param([System.Object]$sender,[System.Windows.RoutedEventArgs]$e)
+		Get-FileName
+		$FileName.Content = [io.path]::GetFileNameWithoutExtension($SelectedFile)
+	})
+	$Trigger.add_Click({
+		param([System.Object]$sender,[System.Windows.RoutedEventArgs]$e)
+		Write-Host "---------Triggering---------"
+		$message = Get-Content -Path $SelectedFile -Raw -ErrorAction SilentlyContinue
+		$statusCode = Invoke-WebRequest -Uri $url -Method Post -Body $message -ContentType $contentType | Select-Object -ExpandProperty StatusCode
+		if($statusCode -eq 202)
+		{
+			$Result.Content = "Accepted"
+			$Result.Foreground ="#f44156"
+		}
+		else
+		{
+			$Result.Content = "Failed"
+			$Result.Foreground ="#3e6307"
+		}
+		Write-Host "-------Triggered Successfully--------"
+	})
+	$Request.add_Checked({
+		param([System.Object]$sender,[System.Windows.RoutedEventArgs]$e)
+		$Global:requestType = $Request.Content
+	})
+	$Manual.add_Checked({
+		param([System.Object]$sender,[System.Windows.RoutedEventArgs]$e)
+		$Global:requestType = $Manual.Content
+	})
+}
+Function Get-FileName()
+{
+    Add-Type -AssemblyName System.Windows.Forms
+    $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+    $OpenFileDialog.Title = "Please Select File"
+    $OpenFileDialog.filter = "XML (*.xml)|*.xml|JSON (*.json)|*.json"
+    # Out-Null supresses the "OK" after selecting the file.
+	$OpenFileDialog.ShowDialog() | Out-Null
+    $Global:SelectedFile = $OpenFileDialog.FileName
+}
+$window = Load-Xaml
+Add-ControlVariables
+Set-EventHandlers
+$window.ShowDialog()
---
+++ PowerShellProject/out/Azure.xaml.ps1
@@ -0,0 +1,29 @@
+﻿function Add-ControlVariables {	
+New-Variable -Name 'Login' -Value $window.FindName('Login') -Scope 1 -Force
+}
+
+function Load-Xaml {
+	[xml]$xaml = [System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String('PFdpbmRvdw0KDQogIHhtbG5zPSJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dpbmZ4LzIwMDYveGFtbC9wcmVzZW50YXRpb24iDQoNCiAgeG1sbnM6eD0iaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93aW5meC8yMDA2L3hhbWwiDQoNCiAgVGl0bGU9Ik1haW5XaW5kb3ciIEhlaWdodD0iMzUwIiBXaWR0aD0iNTI1Ij4NCg0KICAgIDxHcmlkPg0KDQogICAgICAgIDxMYWJlbCBDb250ZW50PSJMYWJlbCIgSG9yaXpvbnRhbEFsaWdubWVudD0iTGVmdCIgTWFyZ2luPSI2OCwzOCwwLDAiIFZlcnRpY2FsQWxpZ25tZW50PSJUb3AiIFdpZHRoPSIxOTciLz4NCg0KICAgICAgICA8QnV0dG9uIHg6TmFtZT0iTG9naW4iIENvbnRlbnQ9IkJ1dHRvbiIgSG9yaXpvbnRhbEFsaWdubWVudD0iTGVmdCIgTWFyZ2luPSIzMDcsNDEsMCwwIiBWZXJ0aWNhbEFsaWdubWVudD0iVG9wIiBXaWR0aD0iNzUiIENsaWNrPSJMb2dpbiIvPg0KDQogICAgPC9HcmlkPg0KDQo8L1dpbmRvdz4='))
+[System.Reflection.Assembly]::LoadWithPartialName('PresentationFramework') | Out-Null
+	$manager = New-Object System.Xml.XmlNamespaceManager -ArgumentList $xaml.NameTable
+	$manager.AddNamespace("x", "http://schemas.microsoft.com/winfx/2006/xaml");
+	$xaml.SelectNodes("//*[@x:Name='Login']", $manager)[0].RemoveAttribute('Click')
+	$xamlReader = New-Object System.Xml.XmlNodeReader $xaml
+	[Windows.Markup.XamlReader]::Load($xamlReader)
+}
+function Set-EventHandlers {
+	$Login.add_Click({
+		param([System.Object]$sender,[System.Windows.RoutedEventArgs]$e)
+		Login($sender,$e)
+		Connect-AzureRmAccount
+	})
+}
+$window = Load-XamlAdd-ControlVariables
+Set-EventHandlers
+
+function Login
+{
+	param($sender, $e)
+}
+
+$window.ShowDialog()
